b340c035cbc40d362f03bad1a2a5efc131032084
Пабло <162823454+Pabloidd@users.noreply.github.com>
2026-02-28 11:46:48 +0300

    Update svelte.config.js

--- frontend/MyResume/svelte.config.js
+++ frontend/MyResume/svelte.config.js
@@ -1,8 +1,12 @@
-import adapter from '@sveltejs/adapter-auto'; 
+import adapter from '@sveltejs/adapter-static';
 
 export default {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    }),
     paths: {
       base: process.env.NODE_ENV === 'production' ? '/MyResume' : ''
     }
