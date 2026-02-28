82a667769bf72cb8436c2036ebad86de699b674e
Пабло <162823454+Pabloidd@users.noreply.github.com>
2026-02-28 16:40:00 +0300

    Update ConfirmRegistrationWindow.svelte

--- frontend/MyResume/src/lib/components/ConfirmRegistrationWindow.svelte
+++ frontend/MyResume/src/lib/components/ConfirmRegistrationWindow.svelte
@@ -1,8 +1,9 @@
 <script>
     import { goto } from "$app/navigation";
+    import { base } from '$app/paths';
 
     function goToMain(){
-        goto('/')
+        goto('{base}/')
     }
 </script>
 
