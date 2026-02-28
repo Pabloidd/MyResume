10ea97fd9fb7864d6746288967f7952865f1dadf
Пабло <162823454+Pabloidd@users.noreply.github.com>
2026-02-28 16:39:44 +0300

    Update CallToActionSection.svelte

--- frontend/MyResume/src/lib/components/CallToActionSection.svelte
+++ frontend/MyResume/src/lib/components/CallToActionSection.svelte
@@ -1,8 +1,9 @@
 <script>
     import { goto } from "$app/navigation";
+    import { base } from '$app/paths';
 
   function goToSignIn(){
-    goto('/SignIn')
+    goto('{base}/SignIn')
   }
 </script>
 
