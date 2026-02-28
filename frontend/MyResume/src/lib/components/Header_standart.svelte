30ec34bf22f02904f35674d2fa918c75c42956f6
Пабло <162823454+Pabloidd@users.noreply.github.com>
2026-02-28 17:20:05 +0300

    Update Header_standart.svelte

--- frontend/MyResume/src/lib/components/Header_standart.svelte
+++ frontend/MyResume/src/lib/components/Header_standart.svelte
@@ -16,8 +16,8 @@
         
       <div class="nav-container">
         <nav class="main-nav">
-            <a href="{base}/search"  class="nav-link"  class:isActive={$page.url.pathname === '/search'}>Поиск Резюме</a>
-            <a href="{base}/profile" class="nav-link " class:isActive={$page.url.pathname === '/profile'}>Личный кабинет</a>
+          <a href="{base}/search" class="nav-link" class:isActive={$page.url.pathname === `${base}/search`}>Поиск Резюме</a>
+          <a href="{base}/profile" class="nav-link" class:isActive={$page.url.pathname === `${base}/profile`}>Личный кабинет</a>
         </nav>
 
         <button type="button" on:click={goToMain} class="logout-btn">
